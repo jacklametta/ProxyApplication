5c018103b9754878c4f9a413a42f60ee1635617d
jacklametta <alessio.nazio@gmail.com>
2015-12-22 11:18:15 +0100

    UDPPKTManager modified (v1.1): - get Methods added - 2 constructors (for creating/extracting data for UDP)

--- app/src/main/java/com/project/sii/proxyapp/UDPPktManager.java
+++ app/src/main/java/com/project/sii/proxyapp/UDPPktManager.java
@@ -11,6 +11,41 @@ public class UDPPktManager extends IPPktManager {
     private ByteBuffer udpPkt;
     static final int SOURCE_PORT_OFFSET = 0;
     static final int DEST_PORT_OFFSET = 2;
+    static final int CHECKSUM_OFFSET = 6;
+    static final int LENGTH_OFFSET = 4;
+
     /* UDP Header length */
     int payloadOffset = 0;
+
+    public UDPPktManager(){
+        super();
+    }
+
+    public UDPPktManager(ByteBuffer pkt){
+        super();
+        this.udpPkt = pkt;
+    }
+
+    /*private ByteBuffer UDPCreation(){
+
+        reutrn
+
+    }*/
+
+    public int getDestinationPort(){
+        return super.getBytesFromPkt(DEST_PORT_OFFSET, 4).getInt();
+    }
+
+    public int getSourcePort(){
+        return super.getBytesFromPkt(SOURCE_PORT_OFFSET, 4).getInt();
+    }
+
+    public PacketChecksum getChecksum(){
+        return new PacketChecksum(getBytesFromPkt(CHECKSUM_OFFSET, 4));
+    }
+
+    public int getLength(){
+        return super.getBytesFromPkt(LENGTH_OFFSET, 4).getInt();
+    }
+
 }
